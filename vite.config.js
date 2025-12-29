802310926bdac99914507dc92931ea8bdfce155f
baitdaoud68-create <baitdaoud68@gmail.com>
2025-12-29 20:53:16 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -12,5 +16,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   },
 })
